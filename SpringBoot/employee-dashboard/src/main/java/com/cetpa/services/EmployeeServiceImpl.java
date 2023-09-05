be84ad273daf8e4d56b2ad82ebfd54d68e8d0687
Harish-Gautam <143887142+Harish-Gautam@users.noreply.github.com>
2023-09-05 09:49:23 +0530

    Add files via upload

---
+++ SpringBoot/employee-dashboard/src/main/java/com/cetpa/services/EmployeeServiceImpl.java
@@ -0,0 +1,37 @@
+package com.cetpa.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cetpa.models.Employee;
+import com.cetpa.repositories.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService 
+{
+	@Autowired private EmployeeRepository repo;
+
+	public List<Employee> getList() 
+	{
+		return repo.findAll();
+	}
+	public void saveRecord(Employee emp) 
+	{
+		repo.save(emp);
+	}
+	public void deleteRecord(int eid) 
+	{
+		repo.deleteById(eid);
+	}
+	public Employee getRecord(int eid) 
+	{
+		return repo.findById(eid).orElse(null);
+		
+	}
+	public void updateRecord(Employee nemp) 
+	{
+		repo.save(nemp);
+	}
+}
